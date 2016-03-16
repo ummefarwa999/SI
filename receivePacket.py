d292f28443b6664be665782080bbea9fcae096f4
Umm-e-Farwa <umm-e-farwa@hotmail.com>
2016-03-16 22:08:11 +0500

    Update receivePacket.py

--- receivePacket.py
+++ receivePacket.py
@@ -2,7 +2,7 @@ import socket
 import sys
 from thread import *
 
-HOST = '---.--.--.---'  #Enter IP Address # Symbolic name meaning all available interfaces
+HOST = '---.--.--.---'  #Enter IP Address between '  '
 PORT = 1300
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
